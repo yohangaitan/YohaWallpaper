801b577584fe0d54d651a129f83acc9bf14b1921
yohangaitan <gaitanyohann@gmail.com>
2026-09-06 21:25:08 -0500

    fix: delete googtrans cookie from both subdomains

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,7 +28,8 @@ export default function Navbar({ onSearch, onSort, activeSort, searchQuery: exte
       document.cookie = 'googtrans=/en/es; path=/'
     } else {
       document.cookie = 'googtrans=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT'
-      document.cookie = 'googtrans=; path=/; domain=' + window.location.hostname + '; expires=Thu, 01 Jan 1970 00:00:00 GMT'
+      document.cookie = "googtrans=; path=/; domain=" + window.location.hostname + "; expires=Thu, 01 Jan 1970 00:00:00 GMT"
+      document.cookie = "googtrans=; path=/; domain=.plyos.me; expires=Thu, 01 Jan 1970 00:00:00 GMT"
     }
     setLang(next)
     window.location.reload()
